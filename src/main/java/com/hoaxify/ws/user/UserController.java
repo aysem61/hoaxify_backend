068c58fceb7a20a362ce23fa6ff724653a731a61
erdemcicek <erdem25123@gmail>
2022-06-26 04:41:29 +0200

    until filetype check backend

--- src/main/java/com/hoaxify/ws/user/UserController.java
+++ src/main/java/com/hoaxify/ws/user/UserController.java
@@ -6,15 +6,21 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hoaxify.ws.error.ApiError;
 import com.hoaxify.ws.shared.CurrentUser;
 import com.hoaxify.ws.shared.GenericResponse;
+import com.hoaxify.ws.user.vm.UserUpdateVM;
 import com.hoaxify.ws.user.vm.UserVM;
 
 @RestController
@@ -43,6 +49,17 @@ public class UserController {
 	@GetMapping("/users/{username}")
 	public UserVM getUser(@PathVariable String username) {
 		User user = userService.getByUsername(username);
+		return new UserVM(user);
+	}
+	
+	@PutMapping("/users/{username}")
+	@PreAuthorize("#username == principal.username")
+	public UserVM updateUser(@Valid @RequestBody UserUpdateVM updatedUser, @PathVariable String username) {
+//		if(!loggedInUser.getUsername().equals(username)) {
+//			ApiError error = new ApiError(403, "Cannot change another users data", "/api/1.0/users/"+username);
+//			return ResponseEntity.status(HttpStatus.FORBIDDEN).body(error);
+//		}
+		User user = userService.updateUser(username, updatedUser);
 		return new UserVM(user);
 	}
 	
--- src/main/java/com/hoaxify/ws/user/UserService.java
+++ src/main/java/com/hoaxify/ws/user/UserService.java
@@ -1,11 +1,20 @@
 package com.hoaxify.ws.user;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Base64;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.hoaxify.ws.error.NotFoundException;
+import com.hoaxify.ws.file.FileService;
+import com.hoaxify.ws.user.vm.UserUpdateVM;
 
 @Service
 public class UserService {
@@ -14,9 +23,12 @@ public class UserService {
 	
 	PasswordEncoder passwordEncoder;
 	
-	public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+	private final FileService fileService;
+	
+	public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, FileService fileService) {
 		this.userRepository = userRepository;
 		this.passwordEncoder = passwordEncoder;
+		this.fileService = fileService;
 	}
 	
 	public void save(User user) {
@@ -40,5 +52,24 @@ public class UserService {
 		}
 		return inDB;
 	}
+
+	public User updateUser(String username, UserUpdateVM updatedUser) {
+		User inDB = getByUsername(username);
+		inDB.setDisplayName(updatedUser.getDisplayName());
+		if(updatedUser.getImage() != null) {
+			String oldImageName = inDB.getImage();
+			//inDB.setImage(updatedUser.getImage());
+			try {
+				String storedFileName = fileService.writeBase64EncodedStringToFile(updatedUser.getImage());
+				inDB.setImage(storedFileName);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			fileService.deleteFile(oldImageName);
+		}
+		return userRepository.save(inDB);
+	}
+
+	
 
 }
