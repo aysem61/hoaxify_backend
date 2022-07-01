968394ffcb6f85bb1aeaba37fd369c5d64ddcc2b
erdemcicek <79759792+erdemcicek@users.noreply.github.com>
2022-07-01 17:03:47 +0200

    Update FileController.java

--- src/main/java/com/hoaxify/ws/file/FileController.java
+++ src/main/java/com/hoaxify/ws/file/FileController.java
@@ -1,6 +1,5 @@
 package com.hoaxify.ws.file;
 
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -10,12 +9,16 @@ public class FileController {
 	
 	private final FileService fileService;
 	
+	
 	public FileController(FileService fileService) {
 		this.fileService = fileService;
 	}
+	
 
 	@PostMapping("/api/1.0/hoax-attachments")
 	public FileAttachment saveHoaxAttachment(MultipartFile file) {
 		return fileService.saveHoaxAttachment(file);
 	}
+	
+	
 }
